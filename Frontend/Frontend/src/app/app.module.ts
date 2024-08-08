5c9c442520bc2652caf76d5d154df23d546ea040
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-08-08 15:43:33 +0100

    Structurer angular project

--- Frontend/Frontend/src/app/app.module.ts
+++ Frontend/Frontend/src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashbordComponent } from './dashbord/dashbord.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { MainContentComponent } from './main-content/main-content.component';
+import { LoginComponent } from './auth/login/login.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashbordComponent,
+    FooterComponent,
+    SidebarComponent,
+    NavbarComponent,
+    MainContentComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
