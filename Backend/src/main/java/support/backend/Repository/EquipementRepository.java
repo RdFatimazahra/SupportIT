34d251175512af5ac105fd6a05c9c0cdf386f1dd
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-08-09 11:56:57 +0100

    Implementer code security Angular

--- Backend/src/main/java/support/backend/Repository/EquipementRepository.java
+++ Backend/src/main/java/support/backend/Repository/EquipementRepository.java
@@ -1,7 +1,13 @@
 package support.backend.Repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import support.backend.Model.Equipement;
 
+import java.util.Optional;
+
 public interface EquipementRepository extends JpaRepository <Equipement,Integer> {
+    Optional<Equipement> findById(Integer id);
+    @Query(value = "select count(*) from Equipement ")
+    long count();
 }
