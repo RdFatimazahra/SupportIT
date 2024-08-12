1037b7747f81844eae993ffb4dd4ce85ec51199a
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-08-12 22:50:31 +0100

    Add interface panne and enum etatPanne

---
+++ Frontend/Frontend/src/app/interfaces/Panne.ts
@@ -0,0 +1,15 @@
+export enum EtatPanne {
+
+    'SIGNALEE',
+    'EN_COURS',
+    'RESOLUE',
+    'NON_REPARABLE'
+
+}
+
+
+export interface Panne {
+    idPanne : number;
+    description : string;
+    etatPanne : EtatPanne;
+}
