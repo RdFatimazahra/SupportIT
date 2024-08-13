7b90473b371f58b6a0849ef2e357ca5dc72fdfef
RdFatimazahra <fatimaezahraradi@gmail.com>
2024-08-13 12:22:52 +0100

    edit login method by Implementing Role-Based Redirection

--- Frontend/Frontend/src/app/auth/login/login.component.ts
+++ Frontend/Frontend/src/app/auth/login/login.component.ts
@@ -25,7 +25,7 @@ export class LoginComponent implements OnInit{
       password: ['', [Validators.required]],
     })
   }
-  submitForm(): void {
+ /* submitForm(): void {
     console.log(this.loginForm.value);
     this.service.login(this.loginForm.value).subscribe(
       (response : Jwt) => {
@@ -34,5 +34,31 @@ export class LoginComponent implements OnInit{
            this.router.navigateByUrl('/dashboard')
         }
     )
-  }
+  }*/
+    submitForm(): void {
+      console.log(this.loginForm.value);
+      this.service.login(this.loginForm.value).subscribe(
+        (response: Jwt) => {
+          const jwToken = response.token;
+          localStorage.setItem('jwt', jwToken);
+          
+          const decodedToken = this.decodeToken(jwToken);
+          const userRole = decodedToken.role; // Adjust based on how the role is stored in the token
+    
+          if (userRole === 'ROLE_ADMIN') {
+            this.router.navigateByUrl('/admin-dashboard');
+          } else if (userRole === 'ROLE_USER') {
+            this.router.navigateByUrl('/user-dashboard');
+          } else {
+            this.router.navigateByUrl('/dashboard'); // Default route
+          }
+        }
+      );
+    }
+    
+    decodeToken(token: string): any {
+      const payload = token.split('.')[1];
+      return JSON.parse(atob(payload));
+    }
+    
 }
